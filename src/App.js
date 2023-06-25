9801220c022567c0b17ba29c285482d3e5f78207
p.a.r <p.a.r.mygit@gmail.com>
2023-06-25 22:05:29 +0330

    add weather file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="container">
-      <h1 className="bg-black">weather app</h1>
-      <h2>weather app</h2>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Weather.css";
+
+export default function weather() {
+  return (
+    <div>
+      <h1>New York</h1>
+      <ul>
+        <li>Sunday 21:15</li>
+        <li>Broken Clouds</li>
+      </ul>
+    </div>
+  );
+}
